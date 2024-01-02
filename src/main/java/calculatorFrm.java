4488ebdcccd636a0085de964633169baf4d1ccb6
Raúl Núñez <raul.nunez101@alu.ulpgc.es>
2024-01-02 23:04:34 +0000

    Maven library downloaded and first user interface model created

---
+++ src/main/java/calculatorFrm.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class calculatorFrm {
+    private JPanel panel1;
+    private JComboBox fromCurrency;
+    private JComboBox toCurrency;
+    private JTextField fromQuantity;
+    private JTextField toQuantity;
+    private JButton calculate;
+
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
